4c7021788c97efa9d61ee5cb736c8c511ddd499a
Stephanie <stefq1111@gmail.com>
2022-01-14 15:14:24 -0600

    initial file upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const joke = document.getElementById('joke');
+const jokeBtn = document.getElementById('joke-btn');
+
+generateJoke();
+
+jokeBtn.addEventListener('click', generateJoke);
+
+async function generateJoke(){
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  joke.innerHTML = data.joke;
+}
+
+
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+// }
